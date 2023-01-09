8f9ffdbdd438e44928bc62622797542c52bfd2ab
Abdi878 <k21071276@kcl.ac.uk>
2023-01-09 15:37:18 +0000

    Create Calculator with Display

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const multiply = (num1,num2) => num1*num2;
 const subtract = (num1,num2) => num1-num2;
 const divide = (num1,num2) => num1/num2;
 const operate = (operator,num1,num2)=>operator(num1,num2);
+
+const addButton = document.getElementById("add")
+const multiplyButton = document.getElementById("mult")
+const subtractButton = document.getElementById("subtract")
+const divideButton = document.getElementById("divide")
+const display = document.querySelector(".display")
+const numbers = document.querySelectorAll(".number")
+numbers.forEach(function(num){
+    num.addEventListener('click',e=>display.textContent=parseInt(num.innerHTML))})
+
+
